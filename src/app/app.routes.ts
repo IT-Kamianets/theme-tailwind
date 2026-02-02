2c871d84437dc2e7cb792ee3352d0a2981550e27
Honchar Denys <crackeraki@gmail.com>
2026-02-02 12:28:23 +0200

    init base routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { List } from './pages/list/list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: Home,
+	},
+	{
+		path: 'list',
+		component: List,
+	},
+];
